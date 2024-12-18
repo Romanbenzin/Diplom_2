6b54ca1efeadfa9e2f26f2f3e8105dd9afbb6070
Roman <mmavrr@mail.ru>
2024-12-18 19:25:23 +0300

    Сделал тест регистрации пользователя

--- data.py
+++ data.py
@@ -4,5 +4,20 @@ ALREADY_EXISTS_USER = {
     "name": "Username"
 }
 
+USER_WITHOUT_EMAIL = {
+    "password": "password",
+    "name": "Username"
+}
 
+USER_WITHOUT_PASS = {
+    "email": "r.shchetnikov@yandex.ru",
+    "name": "Username"
+}
 
+USER_WITHOUT_NAME = {
+    "email": "r.shchetnikov@yandex.ru",
+    "password": "password"
+}
+
+ALREADY_EXISTS_USER_ERROR = "User already exists"
+CREATE_USER_WITHOUT_FIELD_ERROR = "Email, password and name are required fields"
